7a841dfa49a23b2fcf00f0ddef3d37ab9c3a2d8e
Sarthak Gupta <sarthak.ag2001@gmail.com>
2022-02-12 16:26:34 +0530

    add anims for hero carousel

--- lib/components/HeroCarousel.tsx
+++ lib/components/HeroCarousel.tsx
@@ -6,19 +6,20 @@ import { motion } from "framer-motion";
 import img2 from "../assets/carousel/BR RESORT MAIN FRONT PHOTO (1).jpg";
 import img1 from "../assets/carousel/WhatsApp Image 2022-01-11 at 4.54.22 PM.jpeg";
 
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperProps, SwiperSlide } from "swiper/react";
 
 import SwiperCore, { EffectFade, Navigation, Autoplay } from "swiper";
+import { SwiperEvents } from "swiper/types";
 SwiperCore.use([EffectFade, Navigation, Autoplay]);
 
 const autoplayspeed = 6000;
 
-const changeHandler = () => {
-  console.log("changed")
-}
-
 const HeroCarousel = () => {
+  const [slide, setSlide] = useState(1);
 
+  const changeHandler = (e: any) => {
+    setSlide(e.activeIndex);
+  };
   return (
     <>
       <Swiper
@@ -37,6 +38,7 @@ const HeroCarousel = () => {
             image={img2}
             primaryText="FINEST HOTEL COLLECTION"
             scale={"scale-100"}
+            animKey={slide}
           />
         </SwiperSlide>
         <SwiperSlide>
@@ -45,6 +47,7 @@ const HeroCarousel = () => {
             image={img1}
             primaryText="FINEST HOTEL COLLECTION"
             scale={"scale-100"}
+            animKey={slide}
           />
         </SwiperSlide>
       </Swiper>
@@ -58,35 +61,97 @@ const CarouselImage = ({
   primaryText,
   caption,
   image,
-  scale
+  scale,
+  animKey,
 }: {
   primaryText: string;
   caption: string;
   image: StaticImageData;
-  scale: string
-
+  scale: string;
+  animKey: number;
 }) => {
 
-  
+  const fancyTextVariants = {
+    sentence: {
+      hidden: {
+        opacity: 0
+      },
+      visible: {
+        opacity: 1,
+        transition: {
+          delay: 0.5,
+          duration: 1.5
+        }
+      }
+    },
+  }
+
+  const variants = {
+    sentence: {
+      hidden: {
+        opacity: 0,
+      },
+      visible: {
+        opacity: 1,
+        transition: {
+          delay: 0.5,
+          staggerChildren: 0.05,
+        },
+      },
+    },
+    letter: {
+      hidden: {
+        opacity: 0,
+        y: 50,
+      },
+      visible: {
+        opacity: 1,
+        y: 0,
+      },
+    },
+  };
 
   return (
     <>
-      <div className="relative h-[72vh] md:h-[90vh] w-full">
+      <div className="relative h-[72vh] md:h-[90vh] w-full overflow-hidden">
         <div className="absolute top-0 left-0 w-full h-full bg-black z-[1] opacity-50"></div>
-        <Image
-          placeholder="blur"
-          src={image}
-          layout="fill"
-          objectFit="cover"
-          className={"transition-all duration-1000 " + scale}
-          alt="Hotel Image"
-        />
-        <motion.h1 animate={{opacity: 1}} transition={{duration: 1}} initial={{opacity: 0}} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-semibold text-white text-3xl drop-shadow-2xl font-serif text-center z-[2]">
-          {primaryText}
-          <motion.div className="font-fancy text-gray-300 font-normal py-5 text-5xl drop-shadow-md">
-            {caption}
+        <motion.div
+          className="relative h-[72vh] md:h-[90vh] w-full"
+          animate={{
+            scale: 1.1,
+          }}
+          initial={{ scale: 1 }}
+          transition={{ duration: autoplayspeed / 1000 }}
+          key={animKey}
+        >
+          <Image
+            placeholder="blur"
+            src={image}
+            layout="fill"
+            objectFit="cover"
+            className={"transition-all duration-1000 " + scale}
+            alt="Hotel Image"
+          />
+        </motion.div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-semibold text-white text-3xl drop-shadow-2xl font-serif text-center z-[2]" key={animKey}>
+          <motion.div
+            variants={fancyTextVariants.sentence}
+            initial="hidden"
+            animate="visible"
+          >
+            {primaryText}
           </motion.div>
-        </motion.h1>
+          <motion.div
+            className="font-fancy text-gray-300 font-normal py-5 text-5xl drop-shadow-md"
+            variants={variants.sentence}
+            initial="hidden"
+            animate="visible"
+          >
+            {caption.split("").map((char, index) => {
+              return <motion.span key={char + "-" + index} variants={variants.letter}>{char}</motion.span>;
+            })}
+          </motion.div>
+        </div>
       </div>
     </>
   );
