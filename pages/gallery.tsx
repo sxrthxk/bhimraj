372ae1325e2b353246876ec1da071f9a13bbe0fb
Sarthak Gupta <sarthak@vadelabs.com>
2022-03-07 22:35:06 +0530

    wp2

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -7,7 +7,11 @@ import Head from "next/head";
 import { GetStaticProps, GetStaticPropsContext } from "next";
 import Image from "next/image";
 
-const gallery = ({ data }: { data: { id: number; url: string }[] }) => {
+const gallery = ({
+  data,
+}: {
+  data: { id: number; caption: string; url: string }[];
+}) => {
   return (
     <Layout>
       <Head>
@@ -19,14 +23,19 @@ const gallery = ({ data }: { data: { id: number; url: string }[] }) => {
           Gallery
         </FancyText>
         <div className="m-auto flex flex-wrap justify-center relative w-11/12 md:w-4/5  mt-24 gap-4">
-          {data.map(({ id, url }) => (
-            <div key={id} className="w-60 h-60 relative">
-              <Image
-                src={url}
-                objectFit="cover"
-                layout="fill"
-                alt="Gallery Image"
-              />
+          {data.map(({ id, url, caption }) => (
+            <div key={id} className="flex flex-col items-center">
+              <div className="w-60 h-60 relative">
+                <Image
+                  src={url}
+                  objectFit="cover"
+                  layout="fill"
+                  alt="Gallery Image"
+                />
+              </div>
+              <span className="w-full font-sans uppercase text-lg mt-2">
+              {caption}
+              </span>
             </div>
           ))}
         </div>
@@ -41,20 +50,16 @@ export const getStaticProps: GetStaticProps = async (
   context: GetStaticPropsContext
 ) => {
   const data = await (
-    await fetch("https://bhimrajresort.com/wp-json/wp/v2/media")
+    await fetch("https://bhimrajresort.com/wp-json/wp/v2/posts?categories=73&per_page=99")
   ).json();
 
   return {
     props: {
-      data: data.map(
-        (d: {
-          id: any;
-          media_details: { sizes: { medium_large: { source_url: any } } };
-        }) => ({
-          id: d.id,
-          url: d.media_details.sizes.medium_large.source_url,
-        })
-      ),
+      data: data.map((d: any) => ({
+        id: d.id,
+        caption: d.title.rendered,
+        url: d.yoast_head_json.og_image[0].url,
+      })),
     },
   };
 };
