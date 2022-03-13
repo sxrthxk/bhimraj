81161d46a15c43cb0dd8714b1f3d384427f228ef
Sarthak Gupta <sarthak@vadelabs.com>
2022-03-13 20:26:38 +0530

    anim

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -54,26 +54,16 @@ const LoadImage = ({ url }: { url: string }) => {
         src={url}
         objectFit="cover"
         layout="fill"
-        onLoad={() => setLoaded(true)}
+        // onLoad={() => setLoaded(true)}
         alt="Gallery Image"
       />
       {!loaded && (
         <div className="absolute top-0 left-0 w-full h-full bg-white flex items-center justify-center">
-          <svg viewBox="0 0 100 100" className="w-24 h-24">
+          <svg viewBox="0 0 100 100" className="w-24 h-24 animate-spin">
             <path
               fill="#555"
               d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
-            >
-              <animateTransform
-                attributeName="transform"
-                attributeType="XML"
-                type="rotate"
-                dur="1s"
-                from="0 50 50"
-                to="360 50 50"
-                repeatCount="indefinite"
-              />
-            </path>
+            ></path>
           </svg>
         </div>
       )}
